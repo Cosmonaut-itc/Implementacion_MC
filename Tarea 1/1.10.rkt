122e9a742173ff263626e12ad4c3aa9370be0f9a
Felix De Haro <69743996+Cosmonaut-itc@users.noreply.github.com>
2021-03-02 13:14:15 -0600

    Problema 10
    Problema 10 de la tarea 1.

---
+++ Tarea 1/1.10.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(define (positives lst)
+  (if (empty? lst)
+      '()
+      (if (> 0 (car lst))
+          (positives (cdr lst))
+          (cons (car lst) (positives (cdr lst))))))
+
