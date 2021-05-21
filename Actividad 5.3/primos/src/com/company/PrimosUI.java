2831ab31994e36ff54093e85517065a1413c1593
Cosmonaut-itc <felixddhs@outlook.com>
2021-05-20 19:53:11 -0500

    Finished button class.

--- Actividad 5.3/primos/src/com/company/PrimosUI.java
+++ Actividad 5.3/primos/src/com/company/PrimosUI.java
@@ -3,6 +3,8 @@ package com.company;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrimosUI {
     private JButton button1;
@@ -12,7 +14,32 @@ public class PrimosUI {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                System.out.println("Inicio");
+                String entrada = button1.getText();
+                try {
+                    int nHilos = Integer.parseInt(entrada);
+                    System.out.println("Numero de Hilos: " + nHilos);
+                    primoMultiThread[] Hilos = new primoMultiThread[nHilos];
 
+                    for (int i=0 ; i<nHilos; i++) {
+                       // Hilos[i] = new primoMultiThread(int, bool, int, int); *Aqui se inicializa el constructor de la clase con sus parametros
+                        Hilos[i].start();
+                    }
+
+                    for (int i=0; i<nHilos; i++){
+                        try {
+                            Hilos[i].join();
+                        }catch (InterruptedException ex){
+                            Logger.getLogger(PrimosUI.class.getName()).log(Level.SEVERE, null, e);
+                        }
+                    }
+                } catch (NumberFormatException ex){
+                    Logger.getLogger(PrimosUI.class.getName()).log(Level.SEVERE, null, e);
+                }
+
+            }
+            public void formWindowClosing(java.awt.event.WindowEvent event){
+                System.out.println("Terminado");
             }
         });
     }
